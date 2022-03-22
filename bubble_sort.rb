71b75e2e6077a403b4984ec655792dcfc7d226bd
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-22 15:57:34 +0300

    Code clean up.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,3 @@
-# PROBLEM
-# Sort an unorder list of array using bubble sort.
-# Bubble sort- only compares two elements at a time and swaps their position.
-
-# SOLUTION
-# 1- Compare two elements and order the list in ascending
-
-
 def bubble_sort (unorder_list)
 
     temp = 0
