f038573cc405d9d174bbd9f3a2bc75ef8fe05aec
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-29 13:02:31 +0300

    Rubocop code refractoring done.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,27 +1,24 @@
-def bubble_sort (unorder_list)
+def bubble_sort(unorder_list)
+  temp = 0
+  unorder_list.each_index do |index|
+    next unless unorder_list[index + 1] != nil
 
-    temp = 0
-    unorder_list.each_index do |index|
-        if (unorder_list[index+1] != nil)
-            if (unorder_list[index] > unorder_list[index+1])
-                temp = unorder_list[index]
-                unorder_list[index] = unorder_list[index+1]
-                unorder_list[index+1] = temp
-            end
-        end
-    end
+    next unless unorder_list[index] > unorder_list[index + 1]
 
-    # To check if the array is at its final ordered state
+    temp = unorder_list[index]
+    unorder_list[index] = unorder_list[index + 1]
+    unorder_list[index + 1] = temp
+  end
 
-    not_ordered = false
+  # To check if the array is at its final ordered state
 
-    unorder_list.each_index do |index|
-        if (unorder_list[index+1] != nil)
-            if (unorder_list[index] > unorder_list[index+1])
-                not_ordered = true
-            end
-        end
-    end
+  not_ordered = false
 
-    not_ordered ? bubble_sort(unorder_list) : unorder_list
+  unorder_list.each_index do |index|
+    next unless unorder_list[index + 1] != nil
+
+    not_ordered = true if unorder_list[index] > unorder_list[index + 1]
+  end
+
+  not_ordered ? bubble_sort(unorder_list) : unorder_list
 end
